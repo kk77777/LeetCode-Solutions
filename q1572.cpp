7cad67a602d05c4e570cffff175fb5176be8d010
kk77777 <kaushikgattani@gmail.com>
2020-09-07 15:22:02 +0530

    1572. Matrix Diagonal Sum

---
+++ q1572.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int diagonalSum(vector<vector<int>> &mat)
+    {
+        int n = mat.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == j)
+                {
+                    sum += mat[i][j];
+                }
+                if (i == (n - j - 1))
+                {
+                    sum += mat[i][j];
+                }
+            }
+        }
+
+        if (n % 2 != 0)
+        {
+            sum = sum - mat[n / 2][n / 2];
+        }
+
+        return sum;
+    }
+};
