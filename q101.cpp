9248af78afde9abf97c642a91e9e19522b9335c9
kk77777 <kaushikgattani@gmail.com>
2020-10-23 19:40:34 +0530

    101. Symmetric Tree

---
+++ q101.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    bool check(TreeNode *root1, TreeNode *root2)
+    {
+        if (root1 == NULL && root2 == NULL)
+            return true;
+        if (root1 && root2 && root1->val == root2->val)
+        {
+            return check(root1->left, root2->right) && check(root1->right, root2->left);
+        }
+        return false;
+    }
+
+    bool isSymmetric(TreeNode *root)
+    {
+        return check(root, root);
+    }
+};
