072afa472332fd72de022eb5af119431eef4c8ad
kk77777 <kaushikgattani@gmail.com>
2021-01-14 14:17:07 +0530

    75. Sort Colors (Dutch National Flag Algorithm)

---
+++ q75_dutchNationalFlagAlgorithm.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l,h,m;
+        l=m=0;
+        h=nums.size()-1;
+        while(m<=h){
+            switch(nums[m]){
+                case 0:swap(nums[l++],nums[m++]);
+                    break;
+                case 1:m++;
+                    break;
+                case 2:swap(nums[m],nums[h--]);
+                    break;
+            }
+        }
+    }
+};
