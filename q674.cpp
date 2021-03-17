f11bba3d9b416aded5a9a3c23b4638a2cf248d66
kk77777 <kaushikgattani@gmail.com>
2021-03-17 11:15:39 +0530

    647. Palindromic Substrings

--- q674.cpp
+++ q674.cpp
@@ -1,23 +1,30 @@
-if (s.length() == 1)
-        return 1;
-    bool dp[s.length()][s.length()];
-    int cnt = 0;
-    for (int g = 0; g < s.length(); g++)
+class Solution
+{
+public:
+    int countSubstrings(string s)
     {
-        for (int i = 0, j = g; j < s.length(); i++, j++)
+        if (s.length() == 1)
+            return 1;
+        bool dp[s.length()][s.length()];
+        int cnt = 0;
+        for (int g = 0; g < s.length(); g++)
         {
-            if (g == 0)
-                dp[i][j] = true;
-            else if (g == 1)
+            for (int i = 0, j = g; j < s.length(); i++, j++)
             {
-                dp[i][j] = (s[i] == s[j]) ? true : false;
+                if (g == 0)
+                    dp[i][j] = true;
+                else if (g == 1)
+                {
+                    dp[i][j] = (s[i] == s[j]) ? true : false;
+                }
+                else
+                {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]) ? true : false;
+                }
+                if (dp[i][j])
+                    cnt++;
             }
-            else
-            {
-                dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]) ? true : false;
-            }
-            if (dp[i][j])
-                cnt++;
         }
+        return cnt;
     }
-    return cnt;
+};
