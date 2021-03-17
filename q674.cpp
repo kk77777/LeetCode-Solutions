5afbc8a78e8be1aaa9f7a85517a6ae88a4a86430
kk77777 <kaushikgattani@gmail.com>
2021-03-17 11:14:47 +0530

    647. Palindromic Substrings

---
+++ q674.cpp
@@ -0,0 +1,23 @@
+if (s.length() == 1)
+        return 1;
+    bool dp[s.length()][s.length()];
+    int cnt = 0;
+    for (int g = 0; g < s.length(); g++)
+    {
+        for (int i = 0, j = g; j < s.length(); i++, j++)
+        {
+            if (g == 0)
+                dp[i][j] = true;
+            else if (g == 1)
+            {
+                dp[i][j] = (s[i] == s[j]) ? true : false;
+            }
+            else
+            {
+                dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]) ? true : false;
+            }
+            if (dp[i][j])
+                cnt++;
+        }
+    }
+    return cnt;
