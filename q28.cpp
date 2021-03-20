c7f7d535f372e6feeb0d7ad5758cba3de4d0d2f4
kk77777 <kaushikgattani@gmail.com>
2021-03-20 19:01:41 +0530

    28. Implement strStr()

---
+++ q28.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int strStr(string s, string p)
+    {
+        int n = s.length();
+        int m = p.length();
+        for (int i = 0; i <= n - m; i++)
+        {
+            int j;
+            for (j = 0; j < m; j++)
+            {
+                if (s[i + j] != p[j])
+                {
+                    break;
+                }
+            }
+            if (j == m)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
